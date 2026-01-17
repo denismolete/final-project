c39a4cbeff9ecf24a5e2a75f8ababc8a645a7306
denismolete <denis.molete05@e-uvt.ro>
2026-01-17 06:28:57 +0200

    Add User entity with validation and role management

---
+++ User.java
@@ -0,0 +1,66 @@
+package com.example.finalproject.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+
+    @NotNull(message = "Role is required")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
