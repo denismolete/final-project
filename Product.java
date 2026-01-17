a764b5ca9f5eff2eabbda695c71153fc25f77fd1
denismolete <denis.molete05@e-uvt.ro>
2026-01-17 06:27:09 +0200

    Add Product entity with validation constraints

---
+++ Product.java
@@ -0,0 +1,66 @@
+package com.example.finalproject.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Product name cannot be empty")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price must be positive")
+    private Double price;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity must be positive")
+    private Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
