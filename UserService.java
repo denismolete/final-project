ab5d7e7702290890fc8204ddb3ea93d1f479eb1b
denismolete <denis.molete05@e-uvt.ro>
2026-01-17 06:34:19 +0200

    Create UserService to handle user operations
    Implement UserService for user management functionality.

---
+++ UserService.java
@@ -0,0 +1,27 @@
+package com.example.finalproject.service;
+
+import com.example.finalproject.model.User;
+import com.example.finalproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
